24db1be459523abab5d856c09c1779de59553360
Kai Aragaki <adamaragaki@gmail.com>
2023-07-20 01:10:25 -0400

    fix #2 and #3 for fv as well

--- data-raw/cell_fv.R
+++ data-raw/cell_fv.R
@@ -117,4 +117,50 @@ tidy_variants <- function(filtered_variants) {
 
 cell_fv <- tidy_variants(fv)
 
+filter_gbci_genes <- function(tidy_variants) {
+  gbci_genes <- c(
+    "ARID1A", "DDR2", "ELF3", "NOTCH2", "ASXL2", "EPCAM", "MSH2",
+    "MSH6", "RHOB", "ATR", "MLH1", "PIK3CA", "PPARG", "RHOA", "SETD2",
+    "FAT1", "FBXW7", "FGFR3", "APC", "TERT", "CDKN1A", "E2F3", "EGFR",
+    "EZH2", "KMT2C", "PMS2", "CDKN2A", "CDKN2B", "FANCC", "MTAP",
+    "PSIP1", "RXRA", "TSC1", "FGFR2", "PTEN", "ATM", "CCND1", "HRAS",
+    "KMT2A", "ERBB3", "KMT2D", "KRAS", "MDM2", "BRCA2", "KLF5", "RB1",
+    "FOXA1", "ZFP36L1", "CREBBP", "TSC2", "BRCA1", "CDK12", "ERBB2",
+    "KANSL1", "NF1", "TP53", "ERCC2", "KMT2B", "EP300", "KDM6A",
+    "MED12", "STAG2"
+  )
+  tidy_variants |>
+    rowwise() |>
+    dplyr::filter(any(gene %in% gbci_genes)) |>
+    ungroup()
+}
+
+cell_fv <- filter_gbci_genes(cell_fv)
+
+parse_allele_coverage <- function(allele_coverage) {
+  alleles <- strsplit(allele_coverage, ",")[[1]]
+  allele_df <- strsplit(alleles, "=") |>
+    unlist() |>
+    matrix(byrow = TRUE, ncol = 2) |>
+    as.data.frame()
+  colnames(allele_df) <- c("seq", "count")
+  allele_df$count <- as.integer(allele_df$count)
+  allele_df
+}
+
+get_frac_ref_of_coverage <- function(parsed_alleles, ref, coverage) {
+  parsed_alleles[parsed_alleles$seq == ref, ]$count / coverage
+}
+
+cell_ufv <- cell_fv |>
+  rowwise() |>
+  mutate(
+    pct_ref = parse_allele_coverage(allele_coverage) |>
+      get_frac_ref_of_coverage(ref, coverage)
+  )
+
+cell_fv <- cell_fv |> select(-workflow, -MyVariantDefaultDb_hg19, -exac, -polyphen,
+                   -go_20200511, -hg19_exac_1, -hg19_cosmic_82, -hg19_dbsnp_154,
+                   -hg19_omim_20191001, -hg19_refgene_201, -som_pval, -sift)
+
 usethis::use_data(cell_fv, overwrite = TRUE)
